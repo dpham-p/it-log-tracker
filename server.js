20a174d33c40f135c95699cc22d7b14de7c9bc90
dpham-p <donaldpham402@gmail.com>
2020-01-06 16:57:33 -0600

    Initial commit with json-server

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const connectDB = require('mongoose');
+const path = require('path');
+
+const app = express();
+
+// Connect to MongoDB
+connectDB();
+
+// Initialize middleware
+app.use(express.json());
+
+// Define routes
+app.use('/logs', require('./routes/logs.js'));
+app.use('/techs', require('./routes/techs'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('build'));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
+  );
+}
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Backend server started on port ${PORT}`));
